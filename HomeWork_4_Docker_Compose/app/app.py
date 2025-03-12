ba7b45be8bd6b932dacea4bacc540a4bcdb48dba
Timofey Makarov <timotimakarov@gmail.com>
2025-03-12 19:54:48 +0300

    Create ClassWork_1 and HomeWork_4

---
+++ HomeWork_4_Docker_Compose/app/app.py
@@ -0,0 +1,11 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class PostHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        print(f"POST request:\nPath: {self.path}\nHeaders:\n{self.headers}\nBody:\n{post_data.decode('utf-8')}\n")
+        self.send_response(200)
+        self.end_headers()
+
+HTTPServer(('', 3033), PostHandler).serve_forever()
