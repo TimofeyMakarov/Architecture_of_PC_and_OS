9682c13667f08f5adeebeceb7c231620404f5192
anastasia <94540033+shepnst@users.noreply.github.com>
2025-03-21 13:22:58 +0300

    Update app.py

--- HomeWork_4_Docker_Compose/app/app.py
+++ HomeWork_4_Docker_Compose/app/app.py
@@ -1,12 +1,35 @@
+
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
 
 class PostHandler(BaseHTTPRequestHandler):
     def do_POST(self):
+        # Чтение данных из запроса
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
-        print(f"POST request:\nPath: {self.path}\nHeaders:\n{self.headers}\nBody:\n{post_data.decode('utf-8')}\n")
-        self.send_response(200)
-        self.end_headers()
 
-print("ok")
-HTTPServer(('', 3033), PostHandler).serve_forever()
+        # Вывод данных в консоль для отладки
+        print(
+            f"POST request:\nPath: {self.path}\nHeaders:\n{self.headers}\nBody:\n{post_data.decode('utf-8')}\n")
+        if self.path == '/api/TEST':
+            self.send_response(200)  # Код ответа 200 (OK)
+            # Указываем тип содержимого
+            self.send_header('Content-type', 'application/json')
+            self.send_headers('Access-Control-Allow-Origin', '*')
+            self.end_headers()
+            # Тело ответа
+            response = {"status": "OK", "message": "Запрос успешно обработан"}
+            self.wfile.write(json.dumps(response).encode(
+                'utf-8'))  # Отправляем JSON-ответ
+        else:
+            self.send_error(404, "Путь не найден")
+
+
+# Запуск сервера
+if __name__ == "__main__":
+    # Сервер слушает на всех интерфейсах, порт 3033
+    server_address = ('', 3033)
+    httpd = HTTPServer(server_address, PostHandler)
+    print("Сервер запущен на порту 3033...")
+    httpd.serve_forever()
