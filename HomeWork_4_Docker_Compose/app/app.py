81e89f0a57a3fe68face13c4881c9a3ff4be68f5
Timofey Makarov <timotimakarov@gmail.com>
2025-03-24 02:39:38 +0300

    Final commit

--- HomeWork_4_Docker_Compose/app/app.py
+++ HomeWork_4_Docker_Compose/app/app.py
@@ -1,12 +1,171 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+import signal
+import sys
+import psycopg2
+import re
 
 class PostHandler(BaseHTTPRequestHandler):
     def do_POST(self):
+        # Чтение данных из запроса
         content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length)
-        print(f"POST request:\nPath: {self.path}\nHeaders:\n{self.headers}\nBody:\n{post_data.decode('utf-8')}\n")
-        self.send_response(200)
-        self.end_headers()
+        try:
+            post_data = json.loads(self.rfile.read(content_length).decode('utf-8'))
+        except json.JSONDecodeError:
+            self.send_response(400) # Код значит "Bad request"
+            self.send_header('Content-type', 'application/json')
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.end_headers()
+            self.wfile.write(json.dumps({"message": "Invalid JSON"}).encode('utf-8'))
+            return
 
-print("ok")
-HTTPServer(('', 3033), PostHandler).serve_forever()
+        # Вывод данных в консоль для отладки
+        print(
+            f"POST request:\nPath: {self.path}\nHeaders:\n{self.headers}\nBody:\n{post_data}\n")
+        if self.path != '/':
+            self.send_response(404)  # Код значит "Not found"
+            self.send_header('Content-type', 'application/json')
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.end_headers()
+            self.wfile.write(json.dumps({"message": "Not found"}).encode('utf-8'))
+            return
+        # Если нажата кнопка вставки
+        if post_data['command'] == "insert":
+            name = post_data['name']
+            number = post_data['number']
+            notes = post_data['notes']
+            if not is_correct_name(name):
+                self.send_response(400) # Код значит "Bad request"
+                self.send_header('Content-type', 'application/json')
+                self.send_header('Access-Control-Allow-Origin', '*')
+                self.end_headers()
+                self.wfile.write(json.dumps({"message": "Некорректное ФИО: " + name}).encode('utf-8'))  # Отправляем JSON-ответ
+                return
+            if not is_correct_number(number):
+                self.send_response(400) # Код значит "Bad request"
+                self.send_header('Content-type', 'application/json')
+                self.send_header('Access-Control-Allow-Origin', '*')
+                self.end_headers()
+                self.wfile.write(json.dumps({"message": "Некорректный номер: " + number}).encode('utf-8'))
+                return
+            request = "INSERT INTO telephoneDirectory VALUES"
+            request += " ('" + name + "', '" + number + "', '" + notes + "');"
+            print(request)
+            if send_request(request):
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.send_header('Access-Control-Allow-Origin', '*')
+                self.end_headers()
+                self.wfile.write(json.dumps(get_data()).encode('utf-8'))
+                return
+            else:
+                self.send_response(500) # Код значит ошибку на стороне сервера
+                self.send_header('Content-type', 'application/json')
+                self.send_header('Access-Control-Allow-Origin', '*')
+                self.end_headers()
+                self.wfile.write(json.dumps({"message": "Возникла непредвиденная ошибка. Попробуйте повторить добавление."}).encode('utf-8'))
+                return
+        if post_data['command'] == "delete":
+            field = post_data['field']
+            value = post_data['value']
+            request = "DELETE FROM telephoneDirectory WHERE " + field + " = '" + value + "';"
+            if send_request(request):
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.send_header('Access-Control-Allow-Origin', '*')
+                self.end_headers()
+                self.wfile.write(json.dumps(get_data()).encode('utf-8'))
+                return
+            else:
+                self.send_response(500) # Код значит ошибку на стороне сервера
+                self.send_header('Content-type', 'application/json')
+                self.send_header('Access-Control-Allow-Origin', '*')
+                self.end_headers()
+                self.wfile.write(json.dumps({"message": "Возникла непредвиденная ошибка. Попробуйте повторить удаление."}).encode('utf-8'))
+                return
+        if post_data['command'] == "edit":
+            old_number = post_data['old_number']
+            name = post_data['name']
+            number = post_data['number']
+            notes = post_data['notes']
+            request = "UPDATE telephoneDirectory SET name = '" + name + "', number = '" + number + "', notes = '" + notes + "' WHERE number = '" + old_number + "';"
+            if send_request(request):
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.send_header('Access-Control-Allow-Origin', '*')
+                self.end_headers()
+                self.wfile.write(json.dumps(get_data()).encode('utf-8'))
+                return
+            else:
+                self.send_response(500) # Код значит ошибку на стороне сервера
+                self.send_header('Content-type', 'application/json')
+                self.send_header('Access-Control-Allow-Origin', '*')
+                self.end_headers()
+                self.wfile.write(json.dumps({"message": "Возникла непредвиденная ошибка. Попробуйте повторить обновление."}).encode('utf-8'))
+                return
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+        self.wfile.write(json.dumps(get_data()).encode('utf-8'))  # Отправляем JSON-ответ
+
+def shutdown(signum, frame):
+    print("Shutting down server...")
+    httpd.shutdown()
+    sys.exit(0)
+
+# Подключение к PostgreSQL
+def get_db_connection():
+    conn = psycopg2.connect(
+        host="db",  # Имя сервиса в docker-compose.yml
+        database="telephoneDB", # Имя базы данных из .env
+        user="client", # Имя пользователя из .env
+        password="HardPassword" # Пароль из .env
+    )
+    return conn
+
+def get_data():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM telephoneDirectory;')
+    data = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    # Преобразуем данные в словарь
+    result = [{'name': row[0], 'number': row[1], 'notes': row[2]} for row in data]
+    for row in result:
+        print(row['name'], row['number'], row['notes'])
+    return result
+
+def send_request(sql_request):
+    try:
+        with get_db_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(sql_request)
+                return True
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+        return False
+
+def is_correct_name(name):
+    # Регулярное выражение для проверки ФИО на русском языке
+    pattern = r'^[А-ЯЁ][а-яё]*(?:-[А-ЯЁ][а-яё]*)?\s[А-ЯЁ][а-яё]*(?:\s[А-ЯЁ][а-яё]*)?$'
+    return bool(re.match(pattern, name))
+
+def is_correct_number(number):
+    # Регулярное выражение для проверки номера телефона
+    pattern = r'^(\+7|8)\d{10}$'
+    return bool(re.match(pattern, number))
+
+# Запуск сервера
+if __name__ == "__main__":
+    # Сервер слушает на всех интерфейсах, порт 3033
+    server_address = ('', 3033)
+    httpd = HTTPServer(server_address, PostHandler)
+    print("Сервер запущен на порту 3033...")
+
+    signal.signal(signal.SIGTERM, shutdown)
+    signal.signal(signal.SIGINT, shutdown)
+
+    httpd.serve_forever()
---
+++ HomeWork_4_Docker_Compose/nginx/html/script.js
@@ -0,0 +1,135 @@
+function sendRequest(inputMessage) {
+    fetch('http://localhost:8080/api/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: inputMessage
+    })
+    .then(response => {
+        if (!response.ok) {
+            return response.text().then(text => {
+                throw new Error(`Ошибка сервера: ${response.status} - ${text}`);
+            });
+        }
+        return response.json();
+    })
+    .then(data => {
+        const tableBody = document.querySelector('#dataTable tbody');
+        tableBody.innerHTML = '';  // Очистка таблицы
+
+        data.forEach(row => {
+            const tr = document.createElement('tr');
+            tr.innerHTML = `
+                <td>${row.name}</td>
+                <td>${row.number}</td>
+                <td>${row.notes}</td>
+            `;
+            tableBody.appendChild(tr);
+        });
+        console.log('Ответ от сервера:', data);
+        alert('Запрос выполнен!');
+    })
+    .catch(error => {
+        console.error('Ошибка:', error);
+        alert('Произошла ошибка при выполнении запроса.');
+    });
+}
+
+// Всплывающие окна
+
+// Функция для открытия всплывающего окна
+function openModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = 'block';
+}
+
+// Функция для закрытия окна
+function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = 'none';
+}
+
+// Закрытие окна при клике на крестик
+document.querySelectorAll('.close').forEach(closeBtn => {
+    closeBtn.addEventListener('click', function() {
+        const modal = this.closest('.modal');
+        modal.style.display = 'none';
+    });
+});
+
+// Закрытие окна при клике вне его области
+window.addEventListener('click', function(event) {
+    if (event.target.classList.contains('modal')) {
+        event.target.style.display = 'none';
+    }
+});
+
+// Открытие окон при нажатии на кнопки
+document.getElementById('Insert').addEventListener('click', function() {
+    openModal('insertModal');
+});
+
+document.getElementById('Delete').addEventListener('click', function() {
+    openModal('deleteModal');
+});
+
+document.getElementById('Edit').addEventListener('click', function() {
+    openModal('editModal');
+});
+
+// Обработка отправки форм
+document.getElementById('insertForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Собираем значения из полей ввода
+    const inputName = document.getElementById('insertName').value;
+    const inputNumber = document.getElementById('insertNumber').value;
+    const inputNotes = document.getElementById('insertNotes').value;
+    const data = {
+        command: "insert",
+        name: inputName,
+        number: inputNumber,
+        notes: inputNotes
+    };
+    const jsonData = JSON.stringify(data, null, 2);
+    // Выводим JSON в консоль для проверки
+    console.log(jsonData);
+    sendRequest(jsonData);
+    closeModal('insertModal');
+});
+
+document.getElementById('deleteForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const value = document.getElementById('deleteId').value;
+    const column = document.getElementById('deleteColumn').value;
+    const data = {
+        command: "delete",
+        field: column,
+        value: value
+    };
+    const jsonData = JSON.stringify(data, null, 2);
+    console.log(jsonData);
+    sendRequest(jsonData);
+    closeModal('deleteModal');
+});
+
+document.getElementById('editForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const old_number = document.getElementById('editId').value;
+    const name = document.getElementById('editName').value;
+    const number = document.getElementById('editNumber').value;
+    const notes = document.getElementById('editNotes').value;
+    const data = {
+        command: "edit",
+        old_number: old_number,
+        name: name,
+        number: number,
+        notes: notes
+    };
+    const jsonData = JSON.stringify(data, null, 2);
+    console.log(jsonData);
+    sendRequest(jsonData);
+    closeModal('editModal');
+});
+
+sendRequest(JSON.stringify({command: "select"}));
